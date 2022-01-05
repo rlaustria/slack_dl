ad2c9dfba3606d3aa11bb5b1e02ee4aad00e689c
rlaustria <rlaustria@esilicontech.com>
2022-01-05 19:31:19 +0800

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const stripBrackets = (array, cursor) => {
+	array = JSON.stringify(array, null, 5);
+	array = array.substring(0, array.length - 2) + `${cursor ? ',' : ''}` + '\n';
+	array = array.substring(2, array.length);
+	return array;
+};
+
+const writeArrayTofile = (file, data, cursor, next, cb = () => {}) => {
+	data = stripBrackets(data, cursor);
+
+	fs.appendFileSync(file, data, (err) => {
+		if (err) {
+			console.log(`Error writing to file users.json`);
+			console.log(err);
+		}
+	});
+
+	if (cursor) return next();
+	else {
+		fs.appendFileSync(file, ']', (err) => {
+			if (err) {
+				console.log(`Error writing to file user.json`);
+				console.log(err);
+			}
+		});
+		cb();
+		return;
+	}
+};
+
+module.exports.stripBrackets = stripBrackets;
+module.exports.writeArrayTofile = writeArrayTofile;
---
+++ index.js
@@ -0,0 +1,160 @@
+const qs = require('qs');
+const axios = require('axios');
+const fs = require('fs');
+const { mkdir, rm } = require('fs/promises');
+const { eachLimit } = require('async');
+const { writeArrayTofile } = require('./helpers');
+
+const DOWNLOAD_DIR = './download';
+const MESSAGE_DIR = './download/messages';
+const USERS_DIR = './download/users';
+
+let done = 0;
+let total = 0;
+
+//--------------------
+// Credentials
+//--------------------
+
+const token = 'xoxp-160730254213-1825863363414-2911752150468-de68388fb7b2cdf187a678bf15660ca8';
+axios.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+let channelList = [];
+
+async function createDir() {
+	// delete and recreate folders
+	try {
+		await rm(DOWNLOAD_DIR, { recursive: true });
+		await rm(USERS_DIR, { recursive: true });
+		await rm(MESSAGE_DIR, { recursive: true });
+
+		await mkdir(DOWNLOAD_DIR);
+		await mkdir(USERS_DIR);
+		await mkdir(MESSAGE_DIR);
+	} catch (error) {
+		await mkdir(DOWNLOAD_DIR);
+		await mkdir(USERS_DIR);
+		await mkdir(MESSAGE_DIR);
+	}
+}
+
+const getUsers = function (cursor = '') {
+	// query user list and write data to file
+	let file = `${USERS_DIR}/users.json`;
+	fs.access(file, fs.F_OK, (err) => {
+		if (err) {
+			fs.writeFileSync(`${USERS_DIR}/users.json`, '[\n', (err) => {
+				if (err) console.log(`Error creating file users.json`);
+			});
+
+			return axios
+				.get(`https://slack.com/api/users.list?cursor=${cursor}`)
+				.then((res) => res.data)
+				.then((data) => {
+					if (!data.ok) {
+						console.log('Error querying user list');
+						console.log(data);
+						return;
+					} else {
+						const nextCursor = data.response_metadata ? data.response_metadata.next_cursor : '';
+
+						// write message
+						let users = data.members;
+						writeArrayTofile(file, users, nextCursor, () => {
+							getUsers(nextCursor);
+						});
+					}
+				});
+		}
+	});
+};
+
+const getChannelList = function (cursor = '') {
+	// build channel list and create files
+
+	return axios
+		.get(`https://slack.com/api/conversations.list?types=public_channel,private_channel&cursor=${cursor}`)
+		.then((res) => res.data)
+		.then((data) => {
+			if (!data.ok) {
+				console.log('Error querying channel list');
+				console.log(data);
+				return;
+			} else {
+				let channels = data.channels.map((c) => {
+					fs.access(`${MESSAGE_DIR}/${c.name}.json`, fs.F_OK, (err) => {
+						if (err) {
+							fs.writeFileSync(`${MESSAGE_DIR}/${c.name}.json`, '[\n', (err) => {
+								if (err) console.log(`Error creating file ${c.name}.json`);
+							});
+						} else {
+						}
+					});
+					return [c.id, c.name];
+				});
+				channelList = [...channelList, ...channels];
+
+				const nextCursor = data.response_metadata ? data.response_metadata.next_cursor : '';
+
+				if (nextCursor) return getChannelList(nextCursor);
+				else {
+					total = channelList.length;
+					return;
+				}
+			}
+		});
+};
+
+const getChannelMessages = function (channel, cursor = '') {
+	// query messages for channel and write to file
+	let [id, channelName] = channel;
+	let file = `${MESSAGE_DIR}/${channelName}.json`;
+	return axios
+		.post(`https://slack.com/api/conversations.history?cursor=${cursor}`, qs.stringify({ channel: id }))
+		.then((res) => res.data)
+		.then((data) => {
+			if (!data.ok) {
+				console.log(`Error querying messages for ${channelName}`);
+				console.log(data);
+				fs.appendFileSync(file, JSON.stringify(data), (err) => {
+					if (err) {
+						console.log(`Error writing to file ${channelName}.json`);
+						console.log(err);
+					}
+				});
+				return;
+			} else {
+				const nextCursor = data.response_metadata ? data.response_metadata.next_cursor : '';
+
+				// write message
+				let messages = data.messages;
+
+				writeArrayTofile(
+					file,
+					messages,
+					nextCursor,
+					() => {
+						getChannelMessages(channel, nextCursor);
+					},
+					() => {
+						done++;
+						console.log(`Downloading : ${done}/${total} done`);
+					}
+				);
+			}
+		});
+};
+
+async function run() {
+	await createDir();
+	await getUsers();
+	await getChannelList('');
+
+	console.log('Downloading......');
+
+	await eachLimit(channelList, 100, function (channel) {
+		return getChannelMessages(channel);
+	});
+}
+
+run();
